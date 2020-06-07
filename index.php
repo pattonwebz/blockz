3b76eec3d79a0ff40cf6091639af055f11cdd5e1
William Patton <will@pattonwebz.com>
2020-06-07 20:50:49 +0100

    Use slightly different pagination on archives than main blog page

--- index.php
+++ index.php
@@ -12,5 +12,23 @@ if ( have_posts() ) {
 		get_template_part( 'template-parts/content' );
 	}
 }
-paginate_links();
+
+if ( is_archive() ) {
+	?>
+	<div class="paginate_links paginate-ending border-t border-b pt-4 pb-4 mb-2">
+		<?php
+		paginate_links();
+		?>
+	</div>
+	<?php
+} else {
+	?>
+	<div class="post_nav_link paginate-ending border-t border-b pt-4 pb-4 mb-2">
+		<?php
+		posts_nav_link();
+		?>
+	</div>
+	<?php
+}
+
 get_footer();
