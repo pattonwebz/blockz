cec41bcb02af05993d89ec757c13d094b9a65529
William Patton <will@pattonwebz.com>
2020-06-20 17:11:24 +0100

    Fix nav item toggle and wrap it in an IIFE for scoping

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.getElementById( 'nav-toggle' ).onclick = function(){
-	console.log( 'Clicked' );
-	document.getElementById( 'nav-items' ).classList.toggle( 'hidden' );
-}
+/**
+ * Blocks theme scripts
+ *
+ * @since  0.1.0
+ */
+
+(function () {
+	var navToggleButton = document.getElementById( 'nav-toggle' );
+
+	navToggleButton.onclick = function(){
+		var navItemContainer = document.getElementById( 'header-nav-items' );
+		if ( null !== navItemContainer ) {
+			navItemContainer.classList.toggle( 'block' );
+		}
+	}
+})();
